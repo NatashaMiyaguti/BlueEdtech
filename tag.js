1e5a4908d431d427ebfbe1cc8463ecaf1f277a56
chanelym <chanelym@gmail.com>
2021-08-27 16:43:01 -0300

    Delete tag.js

--- tag.js
+++
@@ -1,8 +0,0 @@
-//Este programa solicita dados do usuário para criação de uma etiqueta
-
-let name = prompt('Digite seu nome completo: ');
-let add = prompt('Digite seu endereço: ');
-let cep = prompt('Digite seu CEP: ');
-let tel = prompt('Digite seu telefone: ');
-
-console.log('Você se chama ' +name+ ', atualmente mora em ' +add+ ' no CEP ' +cep+ ' e o telefone para contato é o ' +tel+ '\n');
